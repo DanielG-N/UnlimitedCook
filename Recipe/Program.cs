4c4a546adb8aa8fb5015eb3bd2d43776e4f018b7
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-05 09:13:16 -0600

    recipe fix?

--- Recipe/Program.cs
+++ Recipe/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.Configure<RecipeDatabaseSettings>(builder.Configuration.GetSection("BookStoreDatabase"));
+builder.Services.Configure<RecipeDatabaseSettings>(builder.Configuration.GetSection("RecipeDatabase"));
 builder.Services.AddSingleton<RecipeDB>();
 
 var app = builder.Build();

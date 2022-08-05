8322bd925ca679dd4707b01a05f34dec99fcf3b3
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-05 08:58:38 -0600

    Recipe Mongo setup

---
+++ Recipe/Recipe.cs
@@ -0,0 +1,16 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Recipe
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    public string RecipeName { get; set; } = null!;
+
+    public List<string> Ingredients { get; set; } = null!;
+
+    public List<string> Instructions { get; set; } = null!;
+}
---
+++ Recipe/RecipeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("recipe")]
+public class RecipeController : ControllerBase
+{
+    private readonly RecipeDB _recipeDB;
+
+    public RecipeController(RecipeDB recipeDB) =>
+        _recipeDB = recipeDB;
+
+    
+}

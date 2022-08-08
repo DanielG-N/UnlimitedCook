859003cdcb85f2d3f09881fdc8e131553b8e8626
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-08 09:04:27 -0600

    cleanup

--- Recipe/RecipeController.cs
+++ Recipe/RecipeController.cs
@@ -5,14 +5,14 @@ using MongoDB.Driver;
 [Route("recipe")]
 public class RecipeController : ControllerBase
 {
-    private readonly RecipeDB _recipeDB;
+    private readonly IMongoCollection<Recipe> _recipeDB;
 
     public RecipeController(RecipeDB recipeDB) =>
-        _recipeDB = recipeDB;
+        _recipeDB = recipeDB._recipeCollection;
 
     [HttpGet]
-        public async Task<ActionResult<List<Recipe>>> GetAllItems()
-        {
-            return await _recipeDB._recipeCollection.Find(_ => true).ToListAsync();
-        }
+    public async Task<ActionResult<List<Recipe>>> GetAllRecipes()
+    {
+        return await _recipeDB.Find(_ => true).ToListAsync();
+    }
 }

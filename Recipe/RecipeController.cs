6c2877fff389a4fda7038addbb4391fc9fe3afc9
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-10 09:40:03 -0600

    Recipe CRUD

--- Recipe/RecipeController.cs
+++ Recipe/RecipeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using MongoDB.Bson;
 
 [ApiController]
 [Route("recipe")]
@@ -14,5 +15,38 @@ public class RecipeController : ControllerBase
     public async Task<ActionResult<List<Recipe>>> GetAllRecipes()
     {
         return await _recipeDB.Find(_ => true).ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<IResult> AddRecipe(Recipe recipe)
+    {
+        await _recipeDB.InsertOneAsync(recipe);
+        return Results.Created($"/{recipe.Id}", recipe);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Recipe>> GetItem(string id)
+    {
+        var recipe = await _recipeDB.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        if(recipe == null)
+            return NotFound();
+
+        return Ok(recipe);
+    }
+
+    [HttpPut]
+    public async Task<IResult> UpdateRecipe(Recipe recipe)
+    {
+        await _recipeDB.FindOneAndReplaceAsync(r => r.Id == recipe.Id, recipe);
+        return Results.Created($"/{recipe.Id}", recipe);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IResult> DeleteRecipe(string id)
+    {
+        var recipe = await _recipeDB.FindOneAndDeleteAsync(r => r.Id == id);
+
+        return Results.Ok(recipe);
     }
 }

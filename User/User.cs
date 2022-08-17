2acea03d679108748cab882ff166a61e8caaf9a3
satanscorner <adeniro@student.neumont.edu>
2022-08-17 08:37:40 -0600

    user docker

--- User/User.cs
+++ User/User.cs
@@ -1,7 +1,7 @@
 public class User {
     public long Id { get; set; }
-    public string name { get; set; }
-    public string username { get; set; }
-    public string password { get; set; }
+    public string? name { get; set; }
+    public string? username { get; set; }
+    public string? password { get; set; }
 
 }

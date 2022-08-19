c8d369cf4a4ffc9748ce631bad45bb64103523eb
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-19 08:34:08 -0600

    CookBook + Recipe dockerfiles + eureka

--- Recipe/Program.cs
+++ Recipe/Program.cs
@@ -1,7 +1,13 @@
+using Steeltoe.Discovery.Client;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery.Eureka;
+using Steeltoe.Discovery;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.Configure<RecipeDatabaseSettings>(builder.Configuration.GetSection("RecipeDatabase"));
 builder.Services.AddSingleton<RecipeDB>();
+builder.Services.AddDiscoveryClient(builder.Configuration);
 
 var app = builder.Build();
 app.MapControllers();

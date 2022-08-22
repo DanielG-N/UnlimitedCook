27150449035dfec7020b605ee5cedbdf32d7c475
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-22 08:00:04 -0600

    CORS stuff

--- User/Program.cs
+++ User/Program.cs
@@ -4,12 +4,25 @@ using Steeltoe.Common.Discovery;
 using Steeltoe.Discovery.Eureka;
 using Steeltoe.Discovery;
 
+var  policyName = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDiscoveryClient(builder.Configuration);
 builder.Services.AddDbContext<UserDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("userDb1")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+                    builder =>
+                    {
+                        builder
+                            .WithOrigins("*") // specifying the allowed origin
+                            .WithMethods("*") // defining the allowed HTTP method
+                            .AllowAnyHeader(); // allowing any header to be sent
+                    });
+});
 
 var app = builder.Build();
+app.UseCors(policyName);
 app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");

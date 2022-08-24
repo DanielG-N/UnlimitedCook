a0dd7b23e32607ec14ad476cccce66d4d95c52ba
DanielG-N <dagonzalez@student.neumont.edu>
2022-08-24 08:45:09 -0600

    fix stuff

--- User/User.cs
+++ User/User.cs
@@ -1,5 +1,5 @@
 public class User {
-    public long Id { get; set; }
+    public int Id { get; set; }
     public string? name { get; set; }
     public string? username { get; set; }
     public string? password { get; set; }

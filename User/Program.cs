580414523866b0ed5c2d6d52dc0ae05da7aeb5c4
satanscorner <adeniro@student.neumont.edu>
2022-08-26 08:11:09 -0600

    commit

--- User/Program.cs
+++ User/Program.cs
@@ -6,8 +6,8 @@ using Steeltoe.Discovery;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddDiscoveryClient(builder.Configuration);
 builder.Services.AddDbContext<UserDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("userDb1")));
+builder.Services.AddDiscoveryClient(builder.Configuration);
 
 var app = builder.Build();
 app.MapControllers();
